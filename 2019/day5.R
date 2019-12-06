223585d5e67d555e5954dd121b18f45af6be2b04
fazetu01 <tucker.fazendin@lexisnexisrisk.com>
2019-12-06 15:21:02 -0600

    progress

--- 2019/day5.R
+++ 2019/day5.R
@@ -1,35 +1,57 @@
-op_code_one <- function(x, i) {
-  ai <- x[i + 2] + 1
-  bi <- x[i + 3] + 1
-  resi <- x[i + 4] + 1
-  x[resi] <- x[ai] + x[bi]
+# x Intcode
+# i 0-index of where to start processing
+# op Function to apply to the second and third elements of x[(i + 1):(i + 4)]
+# parameter mode Which mode to use. 0 is positional, 1 is immediate.
+op_code_group4 <- function(x, i, op = `+`, mode = 0) {
+  xsection <- x[(i + 1):(i + 4)]
+  
+  if (mode == 0) { # positional mode
+    i1 <- xsection[2] + 1 # + 1 to convert to 1 index
+    i2 <- xsection[3] + 1
+    resi <- xsection[4] + 1
+    x[resi] <- op(x[i1], x[i2])
+  } else if (mode == 1) {
+    
+  }
+  
   x
 }
 
-op_code_two <- function(x, i) {
-  ai <- x[i + 2] + 1
-  bi <- x[i + 3] + 1
-  resi <- x[i + 4] + 1
-  x[resi] <- x[ai] * x[bi]
+op_code_one <- function(x, i, mode = 0) op_code_group4(x, i, op = `+`, mode = mode)
+
+op_code_two <- function(x, i, mode = 0) op_code_group4(x, i, op = `*`, mode = mode)
+
+op_code_three <- function(x, i, input, mode = 0) {
+  xsection <- x[(i + 1):(i + 2)]
+  
+  if (mode == 0) {
+    resi <- xsection[2] + 1
+    x[resi] <- input
+  } else if (mode == 1) {
+    
+  }
+  
   x
 }
 
-op_code_three <- function(x, i, input) {
-  inputi <- x[i + 2] + 1
-  x[inputi] <- input
-  x
+op_code_four <- function(x, i, mode = 0) {
+  xsection <- x[(i + 1):(i + 2)]
+  
+  if (mode == 0) {
+    resi <- xsection[2] + 1
+    out <- x[resi]
+  } else if (mode == 1) {
+    out <- xsection[2]
+  }
+  
+  out
 }
 
-op_code_four <- function(x, i) {
-  outputi <- x[i + 2] + 1
-  x[outputi]
-}
-
-op_code <- function(x, op, i, input) {
-  if (op == 1) op_code_one(x, i)
-  else if (op == 2) op_code_two(x, i)
-  else if (op == 3) op_code_three(x, i, input)
-  else if (op == 4) op_code_four(x, i)
+op_code <- function(x, op, i, mode = 0, input) {
+  if (op == 1) op_code_one(x, i, mode)
+  else if (op == 2) op_code_two(x, i, mode)
+  else if (op == 3) op_code_three(x, i, mode, input)
+  else if (op == 4) op_code_four(x, i, mode)
 }
 
 move <- function(op) {
@@ -40,15 +62,15 @@ move <- function(op) {
 }
 
 # Intcode computer
-process_code <- function(code) {
+process_code <- function(code, mode = 0) {
   n <- length(code)
   i <- 1
   
   while(TRUE) {
     op <- code[i]
     if (op == 99) break
-    code <- op_code(code, op, i - 1, NA)
-    i <- i + move(op)
+    code <- op_code(x = code, op = op, i = i - 1, mode = mode, input = NA)
+    i <- i + move(op = op)
   }
   
   code
