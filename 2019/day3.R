7fcd3ad260e2d5f9a05a60b81f40a549a07d821a
fazetu01 <tucker.fazendin@lexisnexisrisk.com>
2019-12-12 16:06:32 -0600

    further on day3

--- 2019/day3.R
+++ 2019/day3.R
@@ -1,6 +1,42 @@
+z <- function(mat, pt) {
+  x <- pt[1] + 1
+  y <- nrow(mat) - pt[2]
+  mat[y, x]
+}
+
+`z<-` <- function(mat, pt, value) {
+  x <- pt[1] + 1
+  y <- nrow(mat) - pt[2]
+  mat[y, x] <- value
+  mat
+}
+
+expand_mat <- function(mat, dir) {
+  switch(
+    dir,
+    "R" = cbind(mat, rep("", nrow(mat))),
+    "L" = cbind(rep("", nrow(mat)), mat),
+    "U" = rbind(rep("", ncol(mat)), mat),
+    "D" = rbind(mat, rep("", ncol(mat)))
+  )
+}
+
+# mat <- matrix(1:15, nrow = 3, ncol = 5, byrow = TRUE)
+# pt <- c(0, 0)
+# z(mat, pt) # 11
+# z(mat, pt) <- 999
+# mat
+# z(mat, c(1, 0)) # 12
+# z(mat, c(1, 1)) # 7
+# z(mat, c(3, 0)) # 14
+
+get_direction <- function(code) substr(code, 1, 1)
+
+get_distance <- function(code) as.numeric(substr(code, 2, nchar(code)))
+
 next_point <- function(current_point, code) {
-  direction <- substr(code, 1, 1)
-  distance <- as.numeric(substr(code, 2, nchar(code)))
+  direction <- get_direction(code)
+  distance <- get_distance(code)
   switch(
     direction,
     "R" = current_point[1] <- current_point[1] + distance,
@@ -20,63 +56,71 @@ wire_points <- function(wire, start = c(0, 0)) {
   res
 }
 
-plot_wire <- function(wire, start = c(0, 0), color = "black", first = FALSE) {
-  pts <- wire_points(wire, start)
-  pts_df <- t(data.frame(pts))
-  if (first) {
-    plot(pts_df, type = "l", col = color, xlab = "x", ylab = "y")
-    points(pts_df, col = color)
-  } else {
-    lines(pts_df, col = color)
-    points(pts_df, col = color)
+find_start <- function(grid, o_val = "O") {
+  row <- which(apply(grid, 1, function(row) any(row == o_val)))
+  col <- which(apply(grid, 2, function(col) any(col == o_val)))
+  c(col - 1, nrow(grid) - row)
+}
+
+fill_grid <- function(wire, grid, start = c(0, 0), fill_val = "1", cross_val = "X") {
+  cp <- find_start(grid)
+  
+  for (code in wire) {
+    dir <- get_direction(code)
+    dist <- get_distance(code)
+    
+    if (dir == "R") {
+      for (i in seq_len(dist)) {
+        cp <- c(cp[1] + 1, cp[2])
+        curr_val <- tryCatch(z(grid, cp), error = function(e) NULL)
+        if (is.null(curr_val) | length(curr_val) == 0) grid <- expand_mat(grid, dir)
+        curr_val <- z(grid, cp)
+        if (curr_val == fill_val) next
+        z(grid, cp) <- ifelse(curr_val != "", cross_val, fill_val)
+      }
+    } else if (dir == "L") {
+      for (i in seq_len(dist)) {
+        cp <- c(cp[1] - 1, cp[2])
+        curr_val <- tryCatch(z(grid, cp), error = function(e) NULL)
+        if (is.null(curr_val) | length(curr_val) == 0) grid <- expand_mat(grid, dir)
+        curr_val <- z(grid, cp)
+        if (curr_val == fill_val) next
+        z(grid, cp) <- ifelse(curr_val != "", cross_val, fill_val)
+      }
+    } else if (dir == "U") {
+      for (i in seq_len(dist)) {
+        cp <- c(cp[1], cp[2] + 1)
+        curr_val <- tryCatch(z(grid, cp), error = function(e) NULL)
+        if (is.null(curr_val) | length(curr_val) == 0) grid <- expand_mat(grid, dir)
+        curr_val <- z(grid, cp)
+        if (curr_val == fill_val) next
+        z(grid, cp) <- ifelse(curr_val != "", cross_val, fill_val)
+      }
+    } else if (dir == "D") {
+      for (i in seq_len(dist)) {
+        cp <- c(cp[1], cp[2] - 1)
+        curr_val <- tryCatch(z(grid, cp), error = function(e) NULL)
+        if (is.null(curr_val) | length(curr_val) == 0) grid <- expand_mat(grid, dir)
+        curr_val <- z(grid, cp)
+        if (curr_val == fill_val) next
+        z(grid, cp) <- ifelse(curr_val != "", cross_val, fill_val)
+      }
+    }
   }
+  
+  grid
 }
 
-slope <- function(x1, x2) {
-  (x2[2] - x1[2]) / (x2[1] - x1[1])
+wire1 <- c("R8","U5","L5","D3")
+wire2 <- c("U7","R6","D4","L4")
+grid <- matrix("O", 1, 1)
+grid <- fill_grid(wire1, grid, fill_val = "1")
+grid <- fill_grid(wire2, grid, fill_val = "2")
+grid
+
+dist_o_to_cross <- function(grid) {
+  o <- find_start(grid)
+  n_crosses <- sum(grid == "X")
 }
 
-intercept <- function(x1, x2) {
-  m <- slope(x1, x2)
-  x1[2] - m * x1[1]
-}
 
-points_cross <- function(a1, a2, b1, b2) {
-  a1 <- c(0, 0)
-  a2 <- c(1, 1)
-  b1 <- c(0, 1)
-  b2 <- c(1, 0)
-  
-  
-  
-  am <- slope(a1, a2)
-  ab <- intercept(a1, a2)
-  bm <- slope(b1, b2)
-  bb <- intercept(b1, b2)
-  
-  af <- function(x) am * x + ab
-  bf <- function(x) bm * x + bb
-  
-  
-  
-  
-}
-
-wire1 <- "R8,U5,L5,D3"
-wire2 <- "U7,R6,D4,L4"
-wire1_pts <- wire_points(wire1)
-wire2_pts <- wire_points(wire2)
-
-plot_wire(wire1, color = "red", first = TRUE)
-plot_wire(wire2, color = "blue")
-
-wire1 <- "R75,D30,R83,U83,L12,D49,R71,U7,L72"
-wire2 <- "U62,R66,U55,R34,D71,R55,D58,R83"
-plot_wire(wire1, color = "red", first = TRUE)
-plot_wire(wire2, color = "blue")
-
-input <- readLines("2019/day3-input.txt")
-wire1 <- input[1]
-wire2 <- input[2]
-plot_wire(wire1, color = "red", first = TRUE)
-plot_wire(wire2, color = "blue")
