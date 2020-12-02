fd0b18db917221da6f3c580e04b7b859cf694088
Fazendin <FazeTu01@risk.regn.net>
2020-12-02 07:26:03 -0600

    add day 2

---
+++ y2020/day2.R
@@ -0,0 +1,26 @@
+input <- readLines("y2020/day2-input.txt")
+
+first <- gsub("^(\\d+)-(\\d+) ([a-z]): ([a-z]*)$", "\\1", input)
+last <- gsub("^(\\d+)-(\\d+) ([a-z]): ([a-z]*)$", "\\2", input)
+letter <- gsub("^(\\d+)-(\\d+) ([a-z]): ([a-z]*)$", "\\3", input)
+password <- gsub("^(\\d+)-(\\d+) ([a-z]): ([a-z]*)$", "\\4", input)
+
+# part 1
+is_valid1 <- vapply(seq_along(input), function(i) {
+  pw <- strsplit(password[i], "")[[1]]
+  times <- sum(grepl(letter[i], pw))
+  (as.integer(first[i]) <= times) & (times <= as.integer(last[i]))
+}, logical(1))
+
+sum(is_valid1) # answer 1
+
+# part 2
+is_valid2 <- vapply(seq_along(input), function(i) {
+  pw <- strsplit(password[i], "")[[1]]
+  f <- as.integer(first[i])
+  l <- as.integer(last[i])
+  let <- letter[i]
+  (pw[f] == let & pw[l] != let) | (pw[f] != let & pw[l] == let)
+}, logical(1))
+
+sum(is_valid2) # answer 2
