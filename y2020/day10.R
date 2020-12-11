c123e1585844af0cc0595d78c3583c52b9b29cf3
fazetu01 <tucker.fazendin@lexisnexisrisk.com>
2020-12-11 16:23:34 -0600

    finish 10 start 11

--- y2020/day10.R
+++ y2020/day10.R
@@ -2,99 +2,41 @@
 input <- as.integer(readLines("y2020/day10-input.txt"))
 
 # part 1
-prod(table(diff(c(0, sort(input), max(input) + 3)))) # answer 1
+prod(table(diff(c(0L, sort(input), max(input) + 3L)))) # answer 1
 
 # part 2
-input <- c(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4)
+input <- c(16L, 10L, 15L, 5L, 1L, 11L, 7L, 19L, 6L, 12L, 4L)
+input <- c(28L, 33L, 18L, 42L, 31L, 14L, 46L, 20L, 48L, 47L, 24L, 23L, 49L, 45L, 19L, 38L, 39L, 11L, 1L, 32L, 25L, 35L, 8L, 17L, 7L, 9L, 4L, 2L, 34L, 10L, 3L)
 
 input_to_joltages <- function(input) {
   input_s <- sort(input)
-  c(0, input_s, input_s[length(input_s)] + 3)
+  c(0L, input_s, input_s[length(input_s)] + 3L)
+}
+
+count_n_ones_in_runs <- function(deltas) {
+  n_ones_in_runs <- c(0L)
+  j <- 1L
+  for (i in seq_along(deltas)) {
+    curr_delta <- deltas[i]
+    
+    if (curr_delta == 1L) {
+      n_ones_in_runs[j] <- n_ones_in_runs[j] + 1L
+    } else if (curr_delta == 3L) {
+      j <- j + 1L
+      n_ones_in_runs[j] <- 0L
+    }
+  }
+  
+  n_ones_in_runs
 }
 
 joltages <- input_to_joltages(input)
+deltas <- diff(joltages)
+table(deltas) # fails if deltas are not all 1's or 3's
 
-next_joltages <- function(in_joltage) {
-  in_joltage + c(1, 2, 3)
-}
+n_ones_in_runs <- count_n_ones_in_runs(deltas)
+n_ones_in_runs <- n_ones_in_runs[n_ones_in_runs != 0] # drop 0 runs
 
-possible_next_joltages <- function(in_joltage, joltages) {
-  joltages[joltages %in% next_joltages(in_joltage)]
-}
-
-valid_arrangement <- function(arrangement) {
-  # arrangement is sorted from lowest to highest joltage
-  all(diff(arrangement) <= 3)
-}
-
-# (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
-# (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
-# (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
-# (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
-# (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
-# (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
-# (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
-# (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
-
-explode <- function(l, x) {
-  if (length(x) == 1) {
-    lapply(l, `c`, x)
-  } else {
-    unlist(mapply(function(lst, val) {
-      lapply(lst, `c`, val)
-    }, replicate(length(x), l, simplify = FALSE), x, SIMPLIFY = FALSE), recursive = FALSE)
-  }
-}
-
-arrangements <- function(in_joltage, joltages, paths = list(in_joltage)) {
-  if (in_joltage == joltages[length(joltages)]) {
-    explode(paths, in_joltage)
-  } else {
-    nexts <- possible_next_joltages(in_joltage, joltages)
-    paths <- explode(paths, nexts)
-    lapply(nexts, function(x) {
-      arrangements(x, joltages = joltages, paths = paths)
-    })
-  }
-}
-
-arrangements(joltages[1], joltages)
-
-all_arrangements <- function(joltages) {
-  # longest isn't valid, no shorter can be valid
-  if (!valid_arrangement(joltages)) return(list())
-  
-  arrangements <- list(joltages[1])
-  
-  while(TRUE) {
-    
-  }
-  
-  for (joltage in joltages[-1]) {
-    nxts <- possible_next_joltages(in_joltage = joltage, joltages = joltages)
-    arrangements <- 
-    
-    if (length(nxts) == 1) {
-      path <- c(joltage, nxts)
-    } else {
-      # explode path or something
-    }
-
-    
-  }
-}
-
-
-n_arrangements <- function(input) {
-  length(all_arrangements(input))
-}
-
-all_arrangements(input)
-
-
-possible_next_joltages(joltages[1], joltages)
-possible_next_joltages(joltages[2], joltages)
-
-choices <- lapply(joltages, possible_next_joltages, joltages)
-choices
-prod(lengths(choices[-length(choices)]))
+mults <- c(1, 2, 4, 7) # fails if runs are longer than 5!
+options(scipen = 100)
+prod(mults[n_ones_in_runs])
