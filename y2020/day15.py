033261bc213b1f44177de18abfe2ee33e39f1af3
fazetu01 <tucker.fazendin@lexisnexisrisk.com>
2020-12-16 14:28:22 -0600

    expand day 15 py

--- y2020/day15.py
+++ y2020/day15.py
@@ -1,8 +1,10 @@
+from typing import List
+
 # input = [0, 3, 6]
 input = [17, 1, 3, 16, 19, 0]
 
 class Game:
-    def __init__(self, start):
+    def __init__(self, start: List[int]):
          # last number said
         self.turn = len(start)
         self.last = start[self.turn - 1]
@@ -25,7 +27,11 @@ class Game:
         else:
             self.said_history[self.last].append(self.turn)
 
-    def find_number_on_turn(self, turn):
+    def run_turns(self, turn: int):
+        for i in range(turn):
+            self.next_number()
+
+    def find_number_on_turn(self, turn: int):
         for key, val in self.said_history.items():
             if turn in val:
                 return key
@@ -33,13 +39,11 @@ class Game:
 # part 1
 n = 2_020
 game = Game(input)
-for i in range(n - len(input)):
-    game.next_number()
+game.run_turns(n - len(input))
 game.find_number_on_turn(n)
 
 # part 2
 n = 30_000_000
 game = Game(input)
-for i in range(n - len(input)):
-    game.next_number()
+game.run_turns(n - len(input))
 game.find_number_on_turn(n)
