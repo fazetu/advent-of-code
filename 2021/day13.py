1d5db5bfdbf6bd9f177e2276322c83ff48444404
fazetu <tfazendin17@gmail.com>
2021-12-13 21:34:02 -0600

    finish day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 import copy
 from Grid import IntGrid
 
-input = [
+small_input = [
     "6,10",
     "0,14",
     "9,10",
@@ -22,21 +22,25 @@ input = [
     "8,10",
     "9,0",
     "",
-    "fold along y=5", # "fold along y=7",
+    "fold along y=7",
     "fold along x=5",
 ]
 
-# with open("day13-input.txt", "r") as f:
-#     input = [line.strip() for line in f.readlines()]
+with open("day13-input.txt", "r") as f:
+    input = [line.strip() for line in f.readlines()]
+
 
 def parse_input(input: List[str]) -> Tuple[List[str], List[str]]:
     brk = [i for i, line in enumerate(input) if line == ""][0]
     points = input[:brk]
-    folds = input[(brk + 1):]
+    folds = input[(brk + 1) :]
     return (points, folds)
 
+
 def make_grid(points: List[str]) -> IntGrid:
-    int_points = [(int(x), int(y)) for x, y in [point.split(",") for point in points]]
+    int_points = [
+        (int(x), int(y)) for x, y in [point.split(",") for point in points]
+    ]
     mc = max([x for x, _ in int_points]) + 1
     mr = max([y for _, y in int_points]) + 1
     grid = [[0 for _ in range(mc)] for _ in range(mr)]
@@ -44,6 +48,7 @@ def make_grid(points: List[str]) -> IntGrid:
         x, y = point
         grid[y][x] = 1
     return grid
+
 
 def or_grids(grid1: IntGrid, grid2: IntGrid) -> IntGrid:
     res = []
@@ -59,6 +64,7 @@ def or_grids(grid1: IntGrid, grid2: IntGrid) -> IntGrid:
 
     return res
 
+
 def extend_above(grid: IntGrid, by: int) -> IntGrid:
     g = copy.deepcopy(grid)
     c = len(grid[0])
@@ -67,40 +73,63 @@ def extend_above(grid: IntGrid, by: int) -> IntGrid:
         g.insert(0, row)
     return g
 
+
+def extend_right(grid: IntGrid, by: int) -> IntGrid:
+    g = copy.deepcopy(grid)
+    for row in g:
+        for _ in range(by):
+            row.append(0)
+    return g
+
+
 def make_horizontal_fold(grid: IntGrid, hline: int) -> IntGrid:
     top = grid[:hline]
-    bottom = grid[(hline + 1):]
+    bottom = grid[(hline + 1) :]
     bottom_flipped = list(reversed(bottom))
 
-    if len(top) != len(bottom_flipped):
-        if len(top) > len(bottom_flipped):
+    size_top = len(top)
+    size_bottom = len(bottom_flipped)
+
+    if size_top != size_bottom:
+        if size_top > size_bottom:
             # there are more rows in top than bottom
-            need_n = len(top) - len(bottom_flipped)
+            need_n = size_top - size_bottom
             # add 0 rows to the top of bottom_flipped
             bottom_flipped = extend_above(bottom_flipped, need_n)
         else:
             # there are less rows in top than bottom
-            need_n = len(bottom_flipped) - len(top)
+            need_n = size_bottom - size_top
             # add 0 rows to the top of top
             top = extend_above(top, need_n)
 
     return or_grids(top, bottom_flipped)
 
+
 def make_vertical_fold(grid: IntGrid, vline: int) -> IntGrid:
     left = [row[:vline] for row in grid]
-    right = [row[(vline + 1):] for row in grid]
-    right_flipped = [list(reversed(row)) for row in right]
+    right = [row[(vline + 1) :] for row in grid]
+    left_flipped = [list(reversed(row)) for row in left]
 
-    if len(left[0]) != len(right_flipped[0]):
-        raise ValueError("Left and right are different number of columns")
+    size_left = len(left_flipped[0])
+    size_right = len(right[0])
 
-    return or_grids(left, right_flipped)
+    if size_left != size_right:
+        if size_left > size_right:
+            need_n = size_left - size_right
+            right = extend_right(right, need_n)
+        else:
+            need_n = size_right - size_left
+            left_flipped = extend_right(left_flipped, need_n)
+
+    return or_grids(left_flipped, right)
+
 
 def apply_folds(init_grid: IntGrid, folds: List[str]) -> IntGrid:
     grid = copy.deepcopy(init_grid)
 
     for fold in folds:
         direction, line = fold.replace("fold along ", "").split("=")
+
         if direction == "x":
             grid = make_vertical_fold(grid, vline=int(line))
         elif direction == "y":
@@ -108,19 +137,22 @@ def apply_folds(init_grid: IntGrid, folds: List[str]) -> IntGrid:
 
     return grid
 
+
 def count_dots(grid: IntGrid) -> int:
     return sum([val for row in grid for val in row])
 
-# part 1
+
+#input = small_input
+
 points, folds = parse_input(input)
 init_grid = make_grid(points)
 
-make_horizontal_fold(init_grid, 5)
-make_horizontal_fold(init_grid, 7)
-make_horizontal_fold(init_grid, 9)
-
-quit()
+# part 1
 grid = apply_folds(init_grid, [folds[0]])
-count_dots(grid) # answer part 1
+count_dots(grid)  # answer part 1
 
 # part 2
+grid = apply_folds(init_grid, folds)
+
+import seaborn as sns
+sns.heatmap([list(reversed(row)) for row in grid], xticklabels=False, yticklabels=False, cbar=False)
