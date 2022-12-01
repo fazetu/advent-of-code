9773a16716da3b777b51901edd448193e92eec47
fazetu <tucker.fazendin@lexisnexisrisk.com>
2022-12-01 07:56:59 -0600

    add docstring

--- code/utils.py
+++ code/utils.py
@@ -3,6 +3,17 @@ import os
 
 
 def read_input(day: int) -> List[str]:
+    """Helper to read the given day's input file
+
+    Assumes the input file is in the input folder with the name dayN.txt. Reads
+    all the lines and strips white space from each.
+
+    Args:
+        day (int): Day number to read in.
+
+    Returns:
+        List[str]: Lines in the input file.
+    """
     code_dir = os.path.dirname(__file__)
     input_dir = os.path.normpath(os.path.join(code_dir, "..", "input"))
     input_file = os.path.join(input_dir, f"day{day}.txt")
