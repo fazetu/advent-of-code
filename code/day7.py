475e4392d33324dbe5a1040d0e178b8dda10bd28
fazetu <tucker.fazendin@lexisnexisrisk.com>
2022-12-08 16:52:50 -0600

    adds to day 7

--- code/day7.py
+++ code/day7.py
@@ -1,3 +1,5 @@
+from typing import Any
+import os
 from utils import read_input
 
 # input = [
@@ -28,75 +30,88 @@ from utils import read_input
 
 input = read_input(7)
 
-# possible lines:
-# starts with 
+def dict_insert_add(d: dict[Any, int], k: Any, v: int):
+    # modifies dictionary in place!
+    if k in d:
+        d[k] += v
+    else:
+        d[k] = v
 
-all_paths = set()
+def join_path(*args: str) -> str:
+    return os.path.normpath(os.path.join(*args)).replace("\\", "/")
 
-
-
-def dir_name(d: str) -> str:
-    return d.replace("dir", "").strip()
-
-def size_of_file(f: str) -> int:
-    size, _ = f.split(" ")
-    return int(size)
-
-def get_all_dirs(lines: list[str]) -> set[str]:
-    dirs = set()
+def get_all_sizes(lines: list[str]) -> dict[str, tuple[str, int]]:
+    in_ls = False
+    all_sizes: dict[str, tuple[str, int]] = {}
+    curr_location = []
 
     for line in lines:
         if line.startswith("$ cd"):
+            in_ls = False
             nm = line.replace("$ cd", "").strip()
+            curr_location.append(nm)
+        elif line == "$ ls":
+            in_ls = True
         elif line.startswith("dir"):
-            nm = dir_name(line)
+            if in_ls:
+                nm = line.replace("dir", "").strip()
+                all_sizes[join_path(*curr_location, nm)] = ("dir", 0)
         else:
-            continue
+            if in_ls:
+                size, nm = line.split(" ")
+                all_sizes[join_path(*curr_location, nm)] = ("file", int(size))
 
-        if nm != "..":
-            dirs.add(nm)
+    return all_sizes
 
-    return dirs
+def get_dir_sizes(all_sizes: dict[str, tuple[str, int]]) -> dict[str, int]:
+    dir_sizes: dict[str, int] = {}
 
-def get_contents_of_dir(d: str, lines: list[str]) -> set[str]:
-    contents = set()
-    in_dir = False
+    for path, (type, size) in all_sizes.items():
+        if type == "file":
+            dict_insert_add(dir_sizes, os.path.dirname(path), size)
 
-    for line in lines:
-        if line.startswith(f"$ cd {d}"):
-            in_dir = True
+    return dir_sizes
 
-        if in_dir:
-            if line.startswith("$ ls") or line.startswith(f"$ cd {d}"):
-                continue
-            elif line.startswith("$ cd"):
-                in_dir = False
+def is_top_dir(d: str) -> bool:
+    return d.count("/") == 1
+
+def all_top_dirs(dir_sizes: dict[str, int]) -> bool:
+    nms = list(dir_sizes.keys())
+    return all([is_top_dir(nm) for nm in nms])
+
+def combine_dir_sizes(dir_sizes: dict[str, int], include_orig: bool = True) -> dict[str, int]:
+    orig_dirs: dict[str, int] = {}
+
+    while not all_top_dirs(dir_sizes):
+        new_dir_sizes: dict[str, int] = {}
+
+        for path, size in dir_sizes.items():
+            if not is_top_dir(path):
+                dname = os.path.dirname(path)
+                dict_insert_add(new_dir_sizes, dname, size)
+                dict_insert_add(orig_dirs, path, size)
             else:
-                contents.add(line)
+                dict_insert_add(new_dir_sizes, path, size)
 
-    return contents
+        dir_sizes = new_dir_sizes
 
-def get_size_of_dir(d: str, lines: list[str]) -> int:
-    print(d)
-    contents = get_contents_of_dir(d, lines)
-    files = set(filter(lambda x: not x.startswith("dir"), contents))
-    dirs = contents.difference(files)
-    size = sum([size_of_file(file) for file in files])
-
-    if len(dirs) > 0:
-        for d in dirs:
-            size += get_size_of_dir(dir_name(d), lines)
-        
-    return size
+    if include_orig:
+        return {**dir_sizes, **orig_dirs}
+    else:
+        return dir_sizes
 
 # part 1
-ds = get_all_dirs(input)
-get_contents_of_dir("btc", input)
-get_contents_of_dir("pjts", input)
+all_sizes = get_all_sizes(input)
+dir_sizes = get_dir_sizes(all_sizes)
+comb_sizes = combine_dir_sizes(dir_sizes)
+small_sizes = [size for size in comb_sizes.values() if size <= 100_000]
 
-# ds = ds.difference("/")  # don't include /
-sizes = {d: get_size_of_dir(d, input) for d in ds}
-sum_small = sum([size for size in sizes.values() if size < 100_000])
+print(all_sizes)
+print("")
+print(dir_sizes)
+print("")
+print(comb_sizes)
+print("")
 
 # answer 1
-print(sum_small)
+print(sum(small_sizes))
