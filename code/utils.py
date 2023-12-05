1509a88621be0f18368c82a0ea25e425710a322a
fazetu <tuckerfazendin@gmail.com>
2023-12-04 18:57:27 -0600

    improve read_input utility

--- code/utils.py
+++ code/utils.py
@@ -1,26 +1,39 @@
-from typing import Any, Union
+from typing import Any, Union, Optional
 import os
+import re
+import inspect
 
 
 def _read_input(input_file: str) -> list[str]:
     with open(input_file, "r") as f:
         lines = f.readlines()
-        
+
     return [line.replace("\n", "") for line in lines]
 
 
-def read_input(day: int) -> list[str]:
+def read_input(day: Optional[int] = None) -> list[str]:
     """Helper to read the given day's input file
 
     Assumes the input file is in the input folder with the name dayN.txt. Reads
     all the lines and strips ending new line characters.
 
+    If assuming the day number, assumes the calling file is named dayN.py.
+
     Args:
-        day (int): Day number to read in.
+        day (Optional[int], optional): Day number to read in. If left as None,
+            tries to assume the day number from the calling file. Defaults to
+            None.
 
     Returns:
         List[str]: Lines in the input file.
     """
+    if day is None:
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        calling_filename = module.__file__  # type: ignore
+        s = re.sub("day(\\d+)\\.py", "\\1", os.path.basename(calling_filename))  # type: ignore
+        day = int(s)
+
     code_dir = os.path.dirname(__file__)
     input_dir = os.path.normpath(os.path.join(code_dir, "..", "input"))
     input_file = os.path.join(input_dir, f"day{day}.txt")
